826270c61723784a9b3d275d05b7153ffcf0737b
Jonathan Mirzaei <j.mirzaei@gmail.com>
2024-12-20 00:02:05 +0200

    update configurations file

--- src/public-config.ts
+++ src/public-config.ts
@@ -2,6 +2,6 @@
 export const HOST = process.env.HOST || "https://togglexbackhand-d3gbereja6dkaba6.israelcentral-01.azurewebsites.net";
 export const PORT = process.env.PORT || 443;
 export const USER_PORT = 8080;
-export const API_SERVER_URL = `http://${HOST}:${PORT}/api`;
+export const API_SERVER_URL = `${HOST}:${PORT}/api`;
 
 export const API_ADMIN_URL = `http://${HOST}:${USER_PORT}`;
