610e3e80894db72c57179b7f72fb2aa6825b6c88
Jonathan Mirzaei <j.mirzaei@gmail.com>
2024-12-19 23:32:15 +0200

    update configurations file

--- src/public-config.ts
+++ src/public-config.ts
@@ -1,6 +1,6 @@
 
-export const HOST = process.env.HOST ?? "localhost";
-export const PORT = process.env.PORT ?? 8000;
+export const HOST = process.env.HOST || "localhost";
+export const PORT = process.env.PORT || 8000;
 export const USER_PORT = 8080;
 export const API_SERVER_URL = `http://${HOST}:${PORT}/api`;
 
